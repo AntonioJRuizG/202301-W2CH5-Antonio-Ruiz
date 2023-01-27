9f669fabe3a42f2373911f67f6850051366ab590
AntonioJesusRG <antoniogar129@gmail.com>
2023-01-27 21:55:11 +0100

    add setBoard function

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const board = [];
 const colums = 3;
 const rows = 3;
-for (let i = 0; i < colums; i++) {
-  for (let j = 0; j < rows; j++) {
-    board[i][j] = 0;
+const board = [];
+
+export function setBoard(board, colums, rows) {
+  for (let r = 0; r < colums; r++) {
+    const row = [];
+    for (let c = 0; c < rows; c++) {
+      row.push(0);
+    }
+
+    board.push(row);
   }
+
+  return board;
 }
 
+setBoard(board, colums, rows);
+
+board[1][1] = 1;
+
 console.table(board);
+console.log(board);
